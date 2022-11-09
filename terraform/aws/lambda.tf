70e370850e7fe60d33ecf94bdb2bd6a5e36eae1f
Thomas Plisson <tom@upwindconsulting.net>
2022-11-08 20:18:38 -0500

    2022-11-08

--- terraform/aws/lambda.tf
+++ terraform/aws/lambda.tf
@@ -41,3 +41,22 @@ resource "aws_lambda_function" "analysis_lambda" {
     cost-center = "55010"
   }
 }
+
+data "archive_file" "lambda_zip" {
+    type          = "zip"
+    source_file   = "index.js"
+    output_path   = "lambda_function.zip"
+}
+
+resource "aws_lambda_function" "test_lambda" {
+  filename         = "lambda_function.zip"
+  function_name    = "test_lambda"
+  role             = "${aws_iam_role.iam_for_lambda.arn}"
+  handler          = "index.handler"
+  source_code_hash = "${data.archive_file.lambda_zip.output_base64sha256}"
+  runtime          = "nodejs6.10"
+  tags = {
+    env         = "test"
+    cost-center = "77010"
+  }
+}
