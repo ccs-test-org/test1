187d31e296feca54757c575bc3df382bc1d2d29f
Thomas Plisson <tom@upwindconsulting.net>
2022-11-08 19:25:09 -0500

    2022-11-08

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -81,7 +81,7 @@ resource "aws_s3_bucket" "data_science" {
 }
 
 resource "aws_s3_bucket" "logs" {
-  #checkov:skip=CKV_AWS_20:AWS Access logging not enabled on S3 buckets
+  #checkov:skip=CKV_AWS_18:AWS Access logging not enabled on S3 buckets
   bucket = "${local.resource_prefix.value}-logs"
   acl    = "log-delivery-write"
   versioning {
